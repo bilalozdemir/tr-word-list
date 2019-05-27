29f866f2300a2d7181d306af45c628b1f391b388
Bilal Ozdemir <bilal@kadofir.net>
2019-05-28 01:54:46 +0300

    Add files

---
+++ README.md
@@ -0,0 +1,29 @@
+# A Few Turkish Words
+`words__meanings_big.txt` contains something like 48k words and their meanings _(for now :feelsgood:)_.
+
+The text file is formatted as shown below:
+```
+{word}**{meaning 1}\n{meaning 2}\n%%\n
+```
+
+Usage of the given script has been explained in the docstrings of `scraper.py`.
+
+## Intentions on this Work
+This work intented to be a little source of most-used Turkish words (and meanings of most of them) for any kind of work requires plenty of words and an example of scraping data from a website.
+
+### Used
+* [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) - To scrape couple of websites
+
+* [SQLAlchemy](https://www.sqlalchemy.org/) - To build a fancy :bowtie: database and data model (not pushed here)
+
+* And lots of brute-force :innocent:
+
+### Sources
+* [Wiktionary - Tr](https://www.wiktionary.org.tr) - 48k words
+* [Türk Dil Kurumu](http://sozluk.gov.tr/) - Meanings of 48k words
+
+### Disclaimer (TR)
+5846 sayılı Fikir ve Sanat Eserleri kanununun 35. maddesi (İktibas Serbestisi) doğrultusunda yukarıda belirtilen alenileşmiş eserlerden bazı kısımlar derlenerek ticari amaç güdülmeksizin işlenebilir bir kelime veri tabanı oluşturulmuş olup telif hakkı sahiplerinin umuma arz hakları hiçbir şekilde ihlal edilmemiştir.
+
+### LICENSE
+You can see the license [here.](https://creativecommons.org/licenses/by-sa/4.0/)
---
+++ scraper.py
@@ -0,0 +1,121 @@
+"""
+MEANING SCRAPER SCRIPT
+
+    This script scraps meanings of the words out of the given source and
+    concatenates them together into a '.txt' file.
+
+    Classes
+    -------
+        UserError
+            Catches user errors, instances defined with their error messages
+
+            Values:
+                err: error message
+
+            Functions:
+                __str__: returns error message
+
+    Functions
+    ---------
+        meaning_scraper: adds words to the database
+
+        word_scraper: scraps the data out of a file and put into the database
+
+    USAGE (flags)
+    -------------
+        '--search <word>'
+            looks for meaning(s) of the word on the given url
+
+        '--ml <source txt file> <target txt file>'
+            pulls the words from the source file, finds their meanings and puts
+            them together in the target file
+
+"""
+
+import re
+import sys
+from urllib.parse import quote_plus
+from urllib.request import HTTPCookieProcessor, build_opener
+from bs4 import BeautifulSoup
+
+MEANING_MATCH = re.compile('\"anlam\":\"(.*?)\"')
+OPENURL = build_opener(HTTPCookieProcessor()) #Defining our url opener
+
+class UserError(Exception):
+    """Catching User Errors"""
+
+    def __init__(self, err):
+        super(UserError, self).__init__()
+        self.err = err
+
+    def __str__(self):
+        return self.err
+
+def meaning_scraper(word):
+    """
+    Looks and scrapes the meaning of the word
+
+    ...
+
+    Arguments
+    ----------
+        word: str
+            Word wanted to be searched
+
+    Raises
+    ------
+        UserError:
+            When no meaning is found for the given word
+
+    Returns
+    -------
+        _s: str
+            A formatted string that holds word and the meaning(s) of the word
+    """
+    html = OPENURL.open('http://sozluk.gov.tr/gts?ara={}'.format(quote_plus(word)))
+    soup = BeautifulSoup(html, 'lxml', from_encoding="utf-8")
+    body = soup.find('body')
+    _s = '\n'.join(re.findall(MEANING_MATCH, body.get_text()))
+
+    if not _s:
+        err = 'There is no meaning(s) found for the word: {}'.format(word)
+        raise UserError(err)
+
+    return _s
+
+def make_list(source, target):
+    """
+    Looks and scrapes the meaning of the word
+
+    ...
+
+    Arguments
+    ----------
+        source: str
+            path for source text file
+        target: str
+            path for target text file
+
+    Raises
+    ------
+        UserError:
+            When no meaning is found for the given word in 'meaning_scraper'
+    """
+    with open(source, 'r') as _s:
+        words = _s.read().split('\n')
+    _s.close()
+    with open(target, 'r+') as _t:
+        for word in words:
+            try:
+                _t.write(word + '**' + meaning_scraper(word) + '%%\n')
+            except UserError:
+                _t.write(word + '**0%%\n')
+    _t.close()
+    print('Done!')
+
+
+if __name__ == '__main__':
+    if '--search' in sys.argv:
+        print(meaning_scraper(sys.argv[-1]))
+    if '--ml' in sys.argv:
+        make_list(sys.argv[-2], sys.argv[-1])
